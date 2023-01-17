05fd39e6ef2d736c2a9c7a44b4d75e3160179adf
alla <zebrasteel@gmail.com>
2023-01-17 00:21:57 -0500

    add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :operations
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, uniqueness: true, presence: true, length: { minimum: 1 }
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,7 @@
 class Operation < ApplicationRecord
   belongs_to :category
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :odate, presence: true, length: { minimum: 1 }
+  validates :description, uniqueness: true, presence: true, length: { minimum: 1 }
 end
